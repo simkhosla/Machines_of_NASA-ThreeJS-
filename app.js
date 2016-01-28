a330d186accd6e06f421b7868ba8b3d0c410f552
Simran Khosla <email.simrankhosla@gmail.com>
2016-01-28 16:15:24 -0600

    adding images directory

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(__dirname + "/public/images"));
+app.use('/images', express.static(__dirname + 'public/images'));
 
 
 
