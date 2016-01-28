0f25bea77fb615af20e9f7fe1c41ea06ec1a01ab
Simran Khosla <email.simrankhosla@gmail.com>
2016-01-28 15:27:18 -0600

    mime add

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var mime = require("mime")
 
 
 require('./db/database');
